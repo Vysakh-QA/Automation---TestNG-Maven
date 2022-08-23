cd42c12355bbeacc6c27ef4372854863cc797067
Vysakh-QA <92762424+Vysakh-QA@users.noreply.github.com>
2022-08-23 16:10:02 -0400

    Completed Step 1

---
+++ TestNG Framework for automation/src/test/java/DriverUtil.java
@@ -0,0 +1,26 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class DriverUtil {
+	
+	 static WebDriver driver;
+	  
+
+	 @BeforeTest
+	 public void beforeTest() {
+		 // System.setProperty("webdriver.chrome.driver", "/Users/Vysakh.Prasanth/Downloads/chromedriver");
+		  WebDriverManager.chromedriver().setup();
+
+	 	  driver = new ChromeDriver();
+	 }
+	 
+	 @AfterTest
+	  public void afterTest() {
+		  
+		 driver.quit();
+	  }
+
+}
---
+++ TestNG Framework for automation/src/test/java/GoogleTest.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners(ListenerTest.class)
+public class GoogleTest {
+
+    DriverUtil driverUtil = new DriverUtil();
+
+    @BeforeTest()
+        public void beforeTest() {
+
+            driverUtil.beforeTest();
+
+        }
+
+        @Test
+    public void getURL(){
+        DriverUtil.driver.get("https://www.google.com");
+        DriverUtil.driver.findElement(By.name("q")).sendKeys("Michael Jackson");
+        }
+
+        @Test
+        public void checkLogin(){
+
+        }
+}
